ff777e47c3a7a98683b99d18eb00b0c74ae3af8e
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-04-28 21:29:59 +0530

    city component updated

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,17 +14,21 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const {id} = useParams();
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  const { id } = useParams();
+  const {getCity, currentCity, isLoading } = useCities();
 
+  useEffect(
+    function () {
+      console.log("enter")
+      getCity(id);
+    },
+    [id]
+  );
+
+  
+  if (isLoading) return <Spinner />
   const { cityName, emoji, date, notes } = currentCity;
- 
+
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -53,9 +61,7 @@ function City() {
         </a>
       </div>
 
-      <div>
-        {/* <ButtonBack /> */}
-      </div>
+      <div><BackButton /></div>
     </div>
   );
 }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -7,6 +7,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -27,11 +28,30 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      console.log(id)
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+
+      if (!res.ok) throw new Error("Something went wrong");
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity
       }}
     >
       {children}
@@ -39,10 +59,9 @@ function CitiesProvider({ children }) {
   );
 }
 
-
-function useCities(){
-    const context = useContext(CitiesContext);
-    return context;
+function useCities() {
+  const context = useContext(CitiesContext);
+  return context;
 }
 
-export {CitiesProvider, useCities};
+export { CitiesProvider, useCities };
