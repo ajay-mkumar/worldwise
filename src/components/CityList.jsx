81d4edbc37eaada5dcf7b57f3240d0ecf888cfbd
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-04-28 20:36:20 +0530

    custom context provider added

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,21 +1,25 @@
-import styles from './CityList.module.css'
+import styles from "./CityList.module.css";
 
-import CityItem from './CityItem';
-import Spinner from './Spinner';
-import Message from './Message';
+import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({cities, isLoading}) {
+function CityList() {
+  const { isLoading, cities } = useCities();
+  if (isLoading) return <Spinner />;
 
-    if(isLoading) return <Spinner />
-
-    if (!cities.length)  return <Message message="Add your first city by clicking on a city on the map" />
+  if (!cities.length)
     return (
-        <div className={styles.cityList}>
-           { cities.map((city) => (
-                <CityItem city={city} key={city.id} />
-            ))}
-        </div>
-    )
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  return (
+    <div className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </div>
+  );
 }
 
-export default CityList
+export default CityList;
