5c5c227c6f059f844b0fd666739cda251b8c9dd2
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-04-28 13:53:13 +0530

    product layout and css modules added

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,14 @@
+import style from "./appnav.module.css"
+function AppNav() {
+    return (
+        <div className={style.appnav}>
+            <ul>
+                <li>some</li>
+                <li>some1</li>
+                <li>some2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default AppNav
